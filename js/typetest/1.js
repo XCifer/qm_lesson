ab1d73df053c4c5e339c557f25b1d43892775484
XCifer <13607960126@139.com>
2018-09-28 21:20:36 +0800

    markman

---
+++ js/typetest/1.js
@@ -0,0 +1,16 @@
+var und = undefined;
+var nul = null;
+var boo = true;
+var num = 1;
+var str = "dasd";
+//symbol
+
+// 复杂数据类型（引用类型） Object
+
+var obj = {a:1 };
+var obj1 =obj;
+obj1.a =2 ;
+console.log(obj1);
+console.log()
+
+console.log(typeof und,typeof nul,typeof boo,typeof num,typeof str);
