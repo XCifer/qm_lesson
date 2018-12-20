4cb624f934e7b0a0add6a984091d89ea037e81ba
XCifer <13607960126@139.com>
2018-12-21 01:35:15 +0800

    r exercise

---
+++ r.r
@@ -0,0 +1,96 @@
+bino.gen <- function (samples = 10000, n = 20, pi = 0.5)
+{
+  #在（0，1）中做samples*n次可放回实验。
+  values <- sample(c(0, 1), samples * n, replace = TRUE,
+                   prob = c(1 - pi, pi)) 
+  #创建矩阵，设置列为n
+  value.mat <- matrix(values, ncol = n)
+  #将矩阵value.mat 中的值应用sum函数，边界为1
+  Successes <- apply(value.mat, 1, sum)
+  a1 <- round((table(Successes)/samples), 3)
+  b1 <- round(dbinom(0:n, n, pi), 3)
+  names(b1) <- 0:n
+  hist(Successes, breaks = c((-0.5 + 0):(n + 0.5)), freq = FALSE,
+       ylab = "", col = 13, ylim = c(0, max(a1, b1)),
+       main = " Theoretical Values Superimposed Over Histogram of Simulated Values")
+  x <- 0:n
+  fx <- dbinom(x, n, pi)
+  #在直方图每个条目正中间添加垂直线
+  lines(x, fx, type = "h")
+  #在直方图每个最高点添加实心圆点
+  lines(x, fx, type = "p", pch = 16)
+  list(simulated.distribution = a1, theoretical.distribution = b1)
+} 
+set.seed(31)
+bino.gen(sample=1000,n=5,p=0.5)
+
+#rbinom() looks more like normal distribution
+set.seed(123)
+x<-rbinom(1000,5,0.5)
+table(x)/1000
+
+
+#p(6<=x<=10)
+sum(dbinom(6:10,10,0.33))
+#1-p(x<=5)
+1-pbinom(5,10,0.33)
+#两者互补为全集
+
+
+
+1 - ppois(q = 7, lambda = 4)
+ppois(q = 7, lambda = 4, lower = FALSE)
+ppois(q = 3, lambda = 8)
+ppois(q=3,lambda = 8,lower = TRUE)
+
+?dpois
+
+size <- 100;p<- 0.08;n<- 100
+x<- 0:size
+xx<- round(dbinom(x,n,p),3)
+yy<- round(dpois(x,p*n,1/2),3)
+
+plot(x,yy-xx,type = 'p')
+
+
+
+opar<-  par(no.readonly = TRUE)
+par(mfrow=c(1,2),pty='s')
+x<- 0:15
+px<- dgeom(x,.3)
+plot(x,px,type = 'h')
+xs<-rep(0:15,round(dgeom(0:15,.3)*100000,0))
+#重复1-15中的数，每个对应0-15 p=0.3几何分布100000倍的整数次
+plot(ecdf(xs),main='CDF of X~geom(0.3)',ylab = 'P(X<=x)')
+#ecdf计算经验积累分布函数
+
+
+
+
+opar <- par(no.readonly = TRUE)
+par(mfrow=c(1, 2), pty = "s")
+x <- 0:20
+r <- 6
+px <- dnbinom(x, r, .5)
+plot(x, px, type = "h", xlab = "x", ylab="P(X = x)",
+  main = "PDF of X ~ NB(6, 0.5)")
+xs <- rep(0:20, round(dnbinom(0:20, r, 0.5)*100000, 0))
+plot(ecdf(xs), main = "CDF of X ~ NB(6, 0.5)",
+     ylab = expression(P(X <=x)),
+     xlab = "x")
+par(opar)# reset to original parameters
+
+table(quine$Age)
+xtabs(~Age,data=quine)
+opar <- par(no.readonly = TRUE) # read in current parameters
+par(mfrow=c(2, 2)) # change parameters
+barplot(xtabs(~ Age, data = quine), col = "gray90", xlab = "Age",ylab = "Frequency")
+barplot(prop.table(xtabs(~ Age, data = quine)), col = "gray90", xlab = "Age", ylab = "Relative Frequency")
+par(opar)# reset to original parameters
+
+factorial(7)/(factorial(2))
+
+pnorm(115, 100, 10) - pnorm(90, 100, 10)
+qnorm(0.9, 100, 10)
+qnorm(0.1 + pnorm(105, 100, 10), 100, 10)
+
